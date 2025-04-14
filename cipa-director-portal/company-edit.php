2209355380459c2e6039ce195d36b56fe58d9ba0
Mark Clemence Aratan <aratanmarkclemence@gmail.com>
2025-04-14 11:55:03 +0800

    minor issue fix

--- cipa-director-portal/company-edit.php
+++ cipa-director-portal/company-edit.php
@@ -55,7 +55,7 @@
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">  
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                                    <?php echo $_SESSION['admin']; ?></span>
+                                    <?php echo $_SESSION['CIPA']; ?></span>
                                 <img class="img-profile rounded-circle"
                                     src="../img/undraw_profile.svg">
                             </a>
